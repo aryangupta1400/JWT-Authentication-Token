8661a9749269c6b53c5ecf1d9148c1339fac1ff1
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-11-15 21:53:31 +0530

    Add project files.

---
+++ JWT-Authentication-Token/Models/UserLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Authentication_Token.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ JWT-Authentication-Token/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_Authentication_Token.Models
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
